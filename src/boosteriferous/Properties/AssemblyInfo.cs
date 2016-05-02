70e985c65beb6cd997f606d135311aa890f6fc69
Edward Cree <ec429@cantab.net>
2016-05-02 20:34:12 +0100

    bfer 0.2.0

--- src/boosteriferous/Properties/AssemblyInfo.cs
+++ src/boosteriferous/Properties/AssemblyInfo.cs
@@ -6,17 +6,17 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("")]
 [assembly: AssemblyProduct ("")]
-[assembly: AssemblyCopyright ("Copyright © 2015, soundnfury")]
+[assembly: AssemblyCopyright ("Copyright © 2015-16, soundnfury")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
 
 // Don't include the patch revision in the AssemblyVersion - as this will break any dependent
 // DLLs any time it changes.  Breaking on a minor revision is probably acceptable - it's
 // unlikely that there wouldn't be other breaking changes on a minor version change.
-[assembly: AssemblyVersion("0.1")]
-[assembly: AssemblyFileVersion("0.1.1")]
+[assembly: AssemblyVersion("0.2")]
+[assembly: AssemblyFileVersion("0.2.0")]
 
 // Use KSPAssembly to allow other DLLs to make this DLL a dependency in a
 // non-hacky way in KSP.  Format is (AssemblyProduct, major, minor), and it
 // does not appear to have a hard requirement to match the assembly version.
-[assembly: KSPAssembly("boosteriferous", 0, 1)]
+[assembly: KSPAssembly("boosteriferous", 0, 2)]
