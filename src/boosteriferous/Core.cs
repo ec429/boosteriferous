63bca10b3f9be8f471cb84a2836e7c55c9c39db7
Edward Cree <ec429@cantab.net>
2016-05-05 23:12:49 +0100

    Add 'Progressive' profile, disabled by default
    Realism-overhaul-type mods can enable it and use it as the default.

--- src/boosteriferous/Core.cs
+++ src/boosteriferous/Core.cs
@@ -10,11 +10,14 @@ namespace boosteriferous
 		public abstract string name { get; }
 		public abstract void setFieldVisibility(ModuleControlledFirework mcf);
 		public abstract void recalcCurve(ModuleControlledFirework mcf, out FloatCurve fc, out float timeScale);
+		public bool disable = false;
 		public string techRequired = null;
 		public float costFactor = 0f;
 		public float minThrottleFactor = 1f;
 		public bool isAvailable(ModuleControlledFirework mcf)
 		{
+			if (disable)
+				return false;
 			if (!string.IsNullOrEmpty(techRequired) && ResearchAndDevelopment.GetTechnologyState(techRequired) != RDTech.State.Available)
 				return false;
 			return true;
@@ -76,6 +79,29 @@ namespace boosteriferous
 			fc.Add(1f, timeScale, 0f, 0f);
 		}
 	}
+
+	// Default profile for 'realistic' SRBs: thrust increases over duration of burn
+	public class ProfileProgressive : ProfileShape
+	{
+		public override string name { get { return "Progressive"; } }
+		public override void setFieldVisibility(ModuleControlledFirework mcf)
+		{
+			setFieldInvisible(mcf, "throttleDownPoint");
+			setFieldInvisible(mcf, "throttleDownAmount");
+		}
+		public override void recalcCurve(ModuleControlledFirework mcf, out FloatCurve fc, out float timeScale)
+		{
+			// T = 1 - 0.2P
+			// t = integ[1/(1-0.2P) dP] = (1/0.2) ln (1/(1-0.2P))
+			// so burn time = 5 ln (1.25)
+			Debug.LogFormat("[bfer] Recalculating thrust curve: progressive");
+			// Have to multiply curve points by this to scale maxThrust (almost) correctly
+			timeScale = (float)(5f * Math.Log(1.25));
+			fc = new FloatCurve();
+			fc.Add(0f, timeScale, 0f, -0.2f);
+			fc.Add(1f, timeScale * 0.8f, -0.2f, 0f);
+		}
+	}
 	
 	[KSPAddon(KSPAddon.Startup.Instantly, false)]
 	public class Core : MonoBehaviour
@@ -123,14 +149,9 @@ namespace boosteriferous
 				{
 					ps.techRequired = cn.GetValue("techRequired");
 				}
-				if (cn.HasValue("isDefault"))
-				{
-					int isDefault;
-					if (!int.TryParse(cn.GetValue("isDefault"), out isDefault))
-						Debug.LogWarningFormat("[bfer] Malformed isdefault '{0}' in BoosteriferousProfileShape '{1}'", cn.GetValue("isDefault"), name);
-					else if (isDefault != 0)
-						defaultProfile = name;
-				}
+				bool isDefault = false;
+				if (cn.TryGetValue("isDefault", ref isDefault) && isDefault)
+					defaultProfile = name;
 				if (cn.HasValue("costFactor"))
 				{
 					if (!float.TryParse(cn.GetValue("costFactor"), out ps.costFactor))
@@ -141,6 +162,7 @@ namespace boosteriferous
 					if (!float.TryParse(cn.GetValue("minThrottleFactor"), out ps.minThrottleFactor))
 						Debug.LogWarningFormat("[bfer] Malformed minThrottleFactor '{0}' in BoosteriferousProfileShape '{1}'", cn.GetValue("minThrottleFactor"), name);
 				}
+				cn.TryGetValue("disable", ref ps.disable);
 			}
 		}
 	}
